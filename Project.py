30dfaaf7765d71b0e97fde2c3ef51390390cbc17
Audri-23 <audriahmed2000@gmail.com>
2025-12-30 00:50:05 +0600

    Final

--- Project.py
+++ Project.py
@@ -148,26 +148,6 @@ def draw_shapes():
     for i in obstacles:
         draw_cars(i,i[3],i[4])
 
-    # glPushMatrix()  # Save the current matrix state
-    # glColor3f(1, 0, 0)
-    # glTranslatef(0, 0, 0)  
-    # glutSolidCube(60) # Take cube size as the parameter
-    # glTranslatef(0, 0, 100) 
-    # glColor3f(0, 1, 0)
-    # glutSolidCube(60) 
-
-    # glColor3f(1, 1, 0)
-    # glScalef(2, 2, 2)
-    # gluCylinder(gluNewQuadric(), 40, 5, 150, 10, 10)  # parameters are: quadric, base radius, top radius, height, slices, stacks
-    # glTranslatef(100, 0, 100) 
-    # glRotatef(90, 0, 1, 0)  # parameters are: angle, x, y, z
-    # gluCylinder(gluNewQuadric(), 40, 5, 150, 10, 10)
-
-    # glColor3f(0, 1, 1)
-    # glTranslatef(300, 0, 100) 
-    # gluSphere(gluNewQuadric(), 80, 10, 10)  # parameters are: quadric, radius, slices, stacks
-
-    # glPopMatrix()  # Restore the previous matrix state
 
 
 def keyboardListener(key, x, y):
